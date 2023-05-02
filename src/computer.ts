85097df92d769b6fbe6ea443ad1c64c2bf20f0d6
Denchick3 <124050954+Denchick3@users.noreply.github.com>
2023-05-02 12:02:25 +0300

    Add files via upload

---
+++ src/computer.ts
@@ -0,0 +1,13 @@
+interface Computer {
+    Name: string
+    Proccesor:String
+    Display:Number
+    RAM:Number
+    PSU:Number
+    Storage:Number
+    Src:string
+}
+
+export default Computer;
+
+
